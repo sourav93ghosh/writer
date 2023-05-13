a58894c8e4bf6fe15916871cc021365eb64990c3
sourav93ghosh <sourav93.ghosh@gmail.com>
2023-05-14 00:06:11 +0530

    update readme

--- ArrayUtils.ts
+++ ArrayUtils.ts
@@ -1,7 +1,7 @@
 export class ArrayUtils {
   constructor() {}
 
-  public sumOfArray(arr: Array<number>, initialValue = 0): number {
+  public sumOfArray(arr: Array<number>, initialValue = 0) {
     return arr.reduce((prev, curr) => prev + curr, initialValue);
   }
 
