cd4bea91a81fb0b35c376de3d812750329b6f578
sourav93ghosh <sourav93.ghosh@gmail.com>
2023-05-14 00:01:33 +0530

    removing node_modules

--- NumberUtils.ts
+++ NumberUtils.ts
@@ -1,11 +1,11 @@
 export class NumberUtils {
   constructor() {}
 
-  public isNumericValue(val: any) {
+  public isNumericValue(val: any): boolean {
     return !isNaN(val);
   }
 
-  public isDecimal(val: any) {
+  public isDecimal(val: any): boolean {
     if (!isNaN(val)) {
       const arr = val.split(".");
       return arr.length === 2 && !!arr[0] && !!arr[1];
